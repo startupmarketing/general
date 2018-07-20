5fbfeb687920724961d35b48ffacaae142a7aacc
keglkegl <rkeglevic@gmail.com>
2018-07-20 21:26:39 +0200

    adding blockname on chatfuel api

--- webviews/routes/webviews.js
+++ webviews/routes/webviews.js
@@ -83,6 +83,7 @@ router.get('/schedule-notification/chatfuel', (req, res, next) => {
   const arrival_location = req.query.arrival_location;
   const timezone = req.query.timezone;
   const language = req.query.language;
+  const block_name = req.query.block_name;
   var displayUrl2 = 'https://api.messengerbot.si/webviews/schedule-notification/show?userId=' + userId + '&chatfuel_token=' + chatfuel_token + '&chatfuel_bot_id=' + chatfuel_bot_id + '&first_name=' + first_name + '&last_name=' + last_name + '&arrival_location=' + arrival_location + '&timezone=' + timezone + '&language=' + language + '&block_name=' + block_name;
   res.json(createButtons(displayUrl2)); 
 });
