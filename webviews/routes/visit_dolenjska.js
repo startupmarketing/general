f0ccddb93451b17c5e48706346996ebdd03a139e
startupmarketing <startupmarketing.si@gmail.com>
2018-07-27 09:47:44 +0000

    Adding eng weather webviews for visit dolenjska

--- webviews/routes/visit_dolenjska.js
+++ webviews/routes/visit_dolenjska.js
@@ -5,6 +5,11 @@ const CRNOMELJ_SLO_URL = 'https://api.messengerbot.si/visit-dolenjska/webviews/w
 const NOVO_MESTO_SLO_URL = 'https://api.messengerbot.si/visit-dolenjska/webviews/weather-novo-mesto-slo/show';
 const KOCEVJE_SLO_URL = 'https://api.messengerbot.si/visit-dolenjska/webviews/weather-kocevje-slo/show';
 
+const CRNOMELJ_ENG_URL = 'https://api.messengerbot.si/visit-dolenjska/webviews/weather-crnomelj-eng/show';
+const NOVO_MESTO_ENG_URL = 'https://api.messengerbot.si/visit-dolenjska/webviews/weather-novo-mesto-eng/show';
+const KOCEVJE_ENG_URL = 'https://api.messengerbot.si/visit-dolenjska/webviews/weather-kocevje-eng/show';
+
+
 const createButtonsSlo = () => {
   return {
     messages:[
@@ -47,6 +52,48 @@ const createButtonsSlo = () => {
   ]};
 };
 
+const createButtonsEng = () => {
+  return {
+    messages:[
+      {
+        attachment: {
+          type: 'template',
+          payload: {
+            template_type: 'generic',
+            image_aspect_ratio: 'square',
+            elements: [{
+              title: 'Choose your destination!',
+              subtitle: '',
+              buttons:[
+                {
+                  type: 'web_url',
+                  url: CRNOMELJ_ENG_URL,
+                  title: 'Črnomelj',
+                  messenger_extensions: true,
+                  webview_height_ratio: 'compact'
+                },
+                {
+                  type: 'web_url',
+                  url: NOVO_MESTO_ENG_URL,
+                  title: 'Novo mesto',
+                  messenger_extensions: true,
+                  webview_height_ratio: 'compact'
+                },
+                {
+                  type: 'web_url',
+                  url: KOCEVJE_ENG_URL,
+                  title: 'Kočevje',
+                  messenger_extensions: true,
+                  webview_height_ratio: 'compact'
+                }
+              ]
+            }]
+          }
+        }
+      }
+  ]};
+};
+
 const createCompactButton = (displayUrl) => {
   return {
     messages:[
@@ -75,11 +122,18 @@ const createCompactButton = (displayUrl) => {
   ]};
 };
 
-// <================================CHATFUEL WEATHER TEMPLATE========================>
+// <================================CHATFUEL SLO WEATHER TEMPLATE========================>
 
 router.get('/weather-slo/chatfuel', (req, res, next) => {
   res.json(createButtonsSlo()); 
 });
+
+// <================================CHATFUEL ENG WEATHER TEMPLATE========================>
+
+router.get('/weather-eng/chatfuel', (req, res, next) => {
+  res.json(createButtonsEng()); 
+});
+
 
 //<=============================Weather Črnomelj req/res ============================>
 
@@ -87,6 +141,10 @@ router.get('/weather-slo/chatfuel', (req, res, next) => {
 
 router.get('/weather-crnomelj-slo/show', (req, res, next) => {
   res.sendFile('/var/www/messengerbot.si/api/general/public/visit-dolenjska/weather-crnomelj-slo/index.html');
+});
+
+router.get('/weather-crnomelj-eng/show', (req, res, next) => {
+  res.sendFile('/var/www/messengerbot.si/api/general/public/visit-dolenjska/weather-crnomelj-eng/index.html');
 });
 
 
@@ -105,7 +163,9 @@ router.get('/weather-novo-mesto-slo/show', (req, res, next) => {
   res.sendFile('/var/www/messengerbot.si/api/general/public/visit-dolenjska/weather-novo-mesto-slo/index.html');
 });
 
-
+router.get('/weather-novo-mesto-eng/show', (req, res, next) => {
+  res.sendFile('/var/www/messengerbot.si/api/general/public/visit-dolenjska/weather-novo-mesto-eng/index.html');
+});
 
 router.post('', (req, res, next) => {
   res.status(200).json({
@@ -120,6 +180,11 @@ router.post('', (req, res, next) => {
 router.get('/weather-kocevje-slo/show', (req, res, next) => {
   res.sendFile('/var/www/messengerbot.si/api/general/public/visit-dolenjska/weather-kocevje-slo/index.html');
 });
+
+router.get('/weather-kocevje-eng/show', (req, res, next) => {
+  res.sendFile('/var/www/messengerbot.si/api/general/public/visit-dolenjska/weather-kocevje-eng/index.html');
+});
+
 
 
 
