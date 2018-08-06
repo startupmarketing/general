67df12385a4e4658212119b95f393eb9a161c25f
startupmarketing <startupmarketing.si@gmail.com>
2018-08-06 10:18:12 +0000

    Changing frame picture

--- api/routes/generalApi.js
+++ api/routes/generalApi.js
@@ -73,7 +73,7 @@ router.post('/images', (req, res, next) => {
 		      "attachment": {
 		        "type": "image",
 		        "payload": {
-		          "url": "https://res.cloudinary.com/ddqkeaqnm/image/upload/c_thumb,g_face:auto,h_255,w_255/l_Cloudinary:certificate,y_60/l_text:Georgia_16_bold_center:" + date + ",x_-262,y_-14,co_rgb:594b3C/l_text:Futura_22_italic_center:" + certName + ",y_61,co_rgb:FFFFFF,a_-7/" + fileName
+		          "url": "https://res.cloudinary.com/ddqkeaqnm/image/upload/c_thumb,g_face:auto,h_255,w_255/l_Cloudinary:frame,y_60/l_text:Georgia_16_bold_center:" + date + ",x_-262,y_-14,co_rgb:594b3C/l_text:Futura_22_italic_center:" + certName + ",y_61,co_rgb:FFFFFF,a_-7/" + fileName
 		        }
 		      }
 		    }
