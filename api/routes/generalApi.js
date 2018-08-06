daa7e130a240518fa02a1796d77a49aa5a04adb8
startupmarketing <startupmarketing.si@gmail.com>
2018-08-06 09:08:36 +0000

    Debugging

--- api/routes/generalApi.js
+++ api/routes/generalApi.js
@@ -77,10 +77,20 @@ router.post('/images', (req, res, next) => {
 		        }
 		      }
 		    },
-		    {
-	          "title":"Loved it!",
-	          "block_names": ["Responseimagesblock"]
-        	}
+			{
+		      "text":  "Did you enjoy the last game of the CF Rockets?",
+		      "quick_replies": [
+		        {
+		          "title":"Loved it!",
+		          "block_names": ["Block 1", "Block 2"]
+		        },
+		        {
+		          "title":"Not really...",
+		          "url": "https://rockets.chatfuel.com/api/sad-match",
+		          "type":"json_plugin_url"
+		        }
+		      ]
+		    }
 		  ]
 		}
 );
